9817f706a34361e180a965f26734b7193b1ca2da
carldmitch <carldmitch@gmail.com>
2021-02-28 15:37:48 -0500

    refactor checkbox steps and tests

--- features/step_definitions/checkbox_steps.rb
+++ features/step_definitions/checkbox_steps.rb
@@ -1,55 +1,51 @@
 =begin checkbox_step_1
 When I set the checkbox "attribute=value"
 =end
-When /^I set the checkbox "(\w{2,9})=(.*)"$/ do
-|attribute, value|
-  selector = @browser.checkbox(:"#{attribute}" => value)
-  selector.wait_until(&:present?)
-    if !(selector.set?)
-      selector.set
-    else
-      log("Whoa, wait the checkbox '#{attribute}=#{value}' is already set")
-   end
+When /^I set the checkbox "(.*)"$/ do |selector|
+  element = @browser.checkbox(css: selector)
+  element.present?
+  if !(element.set?)
+    element.set
+  else
+    log("Whoa, wait the checkbox '#{selector}' is already set")
+  end
 end
 
 =begin checkbox_step_2
 When I clear the checkbox "attribute=value"
 =end
-When /^I clear the checkbox "(\w{2,9})=(.*)"$/ do
-|attribute, value|
-  selector = @browser.checkbox(:"#{attribute}" => value)
-  selector.wait_until(&:present?)
-    if selector.set?
-      selector.clear
-    else
-      log("Whoa, wait the checkbox '#{attribute}=#{value}' is already clear")
-    end
+When /^I clear the checkbox "(.*)"$/ do |selector|
+  element = @browser.checkbox(css: selector)
+  element.present?
+  if element.set?
+    element.clear
+  else
+    log("Whoa, wait the checkbox '#{selector}' is already clear")
+  end
 end
 
 =begin checkbox_step_3
 Then the checkbox "attribute=value" should be set
 =end
-Then /^the checkbox "(\w{2,9})=(.*)" should be set$/ do
-|attribute, value|
-  selector = @browser.checkbox(:"#{attribute}" => value)
-  selector.wait_until(&:present?)
-    if selector.set?
-      log("TRUE!!! The checkbox '#{attribute}=#{value}' is set")
-    else
-      fail("FAIL!!!! The checkbox '#{attribute}=#{value}' is not set")
-    end
+Then /^the checkbox "(.*)" should be set$/ do |selector|
+  element = @browser.checkbox(css: selector)
+  element.present?
+  if element.set?
+    log("TRUE!!! The checkbox '#{selector}' is set")
+  else
+    fail("FAIL!!!! The checkbox '#{selector}' is not set")
+  end
 end
 
 =begin checkbox_step_4
 Then the checkbox "attribute=value" should not be set
 =end
-Then /^the checkbox "(\w{2,9})=(.*)" should not be set$/ do
-|attribute, value|
-  selector = @browser.checkbox(:"#{attribute}" => value)
-  selector.wait_until(&:present?)
-    if !(selector.set?)
-      log("TRUE!!! The checkbox '#{attribute}=#{value}' is NOT set")
+Then /^the checkbox "(.*)" should not be set$/ do |selector|
+  element = @browser.checkbox(css: selector)
+  element.present?
+    if !(element.set?)
+      log("TRUE!!! The checkbox '#{selector}' is NOT set")
     else
-      fail("FAIL!!!! the checkbox '#{attribute}=#{value}' IS set")
+      fail("FAIL!!!! the checkbox '#{selector}' IS set")
     end
 end
--- features/step_tests/checkbox_tests.feature
+++ features/step_tests/checkbox_tests.feature
@@ -7,32 +7,32 @@ Feature: Tests for checkbox_steps
   @checkbox_step_1
   Scenario: 1 When I set the checkbox "(.*)"
     Given I am on the "http://www.ranorex.com/web-testing-examples/" page
-    Then the checkbox "id=testcheckbox" should not be set
-    When I set the checkbox "id=testcheckbox"
-    Then the checkbox "id=testcheckbox" should be set
+    Then the checkbox "#testcheckbox" should not be set
+    When I set the checkbox "#testcheckbox"
+    Then the checkbox "#testcheckbox" should be set
 
   @checkbox_step_2
   Scenario: 2 When I clear the checkbox "(.*)"
     Given I am on the "http://www.ranorex.com/web-testing-examples/" page
-    When I set the checkbox "id=testcheckbox"
-    Then the checkbox "id=testcheckbox" should be set
-    When I clear the checkbox "id=testcheckbox"
-    When I clear the checkbox "id=testcheckbox"
-    Then the checkbox "id=testcheckbox" should not be set
+    When I set the checkbox "#testcheckbox"
+    Then the checkbox "#testcheckbox" should be set
+    When I clear the checkbox "#testcheckbox"
+    When I clear the checkbox "#testcheckbox"
+    Then the checkbox "#testcheckbox" should not be set
 
   @checkbox_step_3
   Scenario: 3 Then the checkbox "(.*)" should be set
     Given I am on the "http://www.ranorex.com/web-testing-examples/" page
-    Then the checkbox "id=testcheckbox" should not be set
-    When I set the checkbox "id=testcheckbox"
-    Then the checkbox "id=testcheckbox" should be set
+    Then the checkbox "#testcheckbox" should not be set
+    When I set the checkbox "#testcheckbox"
+    Then the checkbox "#testcheckbox" should be set
 
   @checkbox_step_4
   Scenario: 4 Then the checkbox "(.*)" should not be set
     Given I am on the "http://www.ranorex.com/web-testing-examples/" page
-    Then the checkbox "id=testcheckbox" should not be set
-    When I set the checkbox "id=testcheckbox"
-    Then the checkbox "id=testcheckbox" should be set
+    Then the checkbox "#testcheckbox" should not be set
+    When I set the checkbox "#testcheckbox"
+    Then the checkbox "#testcheckbox" should be set
 
 # if you make any changes to the checkbox_steps.rb you should run the following test
 #
